289764d06d27043a5ba78498fad91a426c9cdf9a
David Smith <dbsmith.dbs83@gmail.com>
2016-08-02 20:30:37 -0700

    fixed localhost thing for link

--- js/view/chart.js
+++ js/view/chart.js
@@ -7,12 +7,11 @@ var link;
 var baseUrl = 'http://' + window.location.hostname;
 
 function createLink(rData, oldData) {
-  //ex: 'http://localhost:3000/share/Trump?sent1=negative&s1=3&p1=2&n1=3&neu1=5&t2=Hillary&sent2=positive&s2=5&p2=5&n2=3&neu2=5'
   //create shareable link
-
+  //ex: 'http://localhost:3000/share/Trump?sent1=negative&s1=3&p1=2&n1=3&neu1=5&t2=Hillary&sent2=positive&s2=5&p2=5&n2=3&neu2=5'
+  if (baseUrl = 'localhost') {baseUrl = 'localhost:3000';}
   var shareString = baseUrl + '/share/' + rData[5] + '?' + 'sent1=' + rData[6] + '&s1=' + rData[0] + '&p1=' + rData[1] + '&n1=' + rData[2] + '&neu1=' + rData[3] + '&t2=' + oldData[5] + '&sent2=' + oldData[6] + '&s2=' + oldData[0] + '&p2=' + oldData[1] + '&n2=' + oldData[2] + '&neu2=' + oldData[3];
 
-  console.log('share string: ', shareString);
   return shareString;
 }
 
